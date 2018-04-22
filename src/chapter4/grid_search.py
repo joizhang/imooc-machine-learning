e7b6f8add1f4376e7ccbbec2bdc6598bd7c200ed
joizhang <zxlwhom@outlook.com>
2018-04-22 09:53:20 +0800

    hyper parameter k

---
+++ src/chapter4/grid_search.py
@@ -0,0 +1,43 @@
+"""
+4-6 网格搜索与 k 近邻算法中更多超参数
+"""
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=666)
+sk_knn_clf = KNeighborsClassifier(n_neighbors=4, weights="uniform")
+sk_knn_clf.fit(X_train, y_train)
+sk_knn_clf.score(X_test, y_test)
+
+# Grid Search
+param_grid = [
+    {
+        'weights': ['uniform'],
+        'n_neighbors': [i for i in range(1, 11)]
+    },
+    {
+        'weights': ['distance'],
+        'n_neighbors': [i for i in range(1, 11)],
+        'p': [i for i in range(1, 6)]
+    }
+]
+knn_clf = KNeighborsClassifier()
+
+if __name__ == "__main__":
+    grid_search = GridSearchCV(knn_clf, param_grid, n_jobs=-1, verbose=2)
+    grid_search.fit(X_train, y_train)
+    # 网格搜索中的最佳分类器
+    print(grid_search.best_estimator_)
+    # 最佳分类器的准确度
+    print(grid_search.best_score_)
+    # 最佳参数
+    print(grid_search.best_params_)
+
+
+
--- src/chapter4/hyper_parameter_k.py
+++ src/chapter4/hyper_parameter_k.py
@@ -1,5 +1,6 @@
 """
 4-4 分类准确度
+4-5 超参数
 """
 import matplotlib.pyplot as plt
 import sklearn.model_selection as sk_model_selection
@@ -25,7 +26,7 @@ some_digit_image = some_digit.reshape(8, 8)
 # plt.imshow(some_digit_image, cmap=matplotlib.cm.binary)
 # plt.show()
 
-# 自定义
+# 自定义的 accuracy_score
 X_train, X_test, y_train, y_test = my_model_selection.train_test_split(X, y, test_ratio=0.2)
 my_knn_clf = knn.KNNClassifier(k=3)
 my_knn_clf.fit(X_train, y_train)
@@ -39,3 +40,47 @@ knn_clf = KNeighborsClassifier(n_neighbors=3)
 knn_clf.fit(X_train, y_train)
 y_predict = knn_clf.predict(X_test)
 print(accuracy_score(y_test, y_predict))
+
+# 寻找最好的 k，如果在边界，需要继续寻找
+best_score = 0.0
+best_k = -1
+for k in range(1, 11):
+    knn_clf = KNeighborsClassifier(n_neighbors=k)
+    knn_clf.fit(X_train, y_train)
+    score = knn_clf.score(X_test, y_test)
+    if score > best_score:
+        best_k = k
+        best_score = score
+print("best_k = ", best_k, "\nbest_score = ", best_score)
+
+# 考虑欧拉距离
+print("\n考虑欧拉距离")
+best_method = ""
+best_score = 0.0
+best_k = -1
+for method in ["uniform", "distance"]:
+    for k in range(1, 11):
+        knn_clf = KNeighborsClassifier(n_neighbors=k, weights=method)
+        knn_clf.fit(X_train, y_train)
+        score = knn_clf.score(X_test, y_test)
+        if score > best_score:
+            best_k = k
+            best_score = score
+            best_method = method
+print("best_k = ", best_k, "\nbest_score = ", best_score, "\nbest_method = ", best_method)
+
+# 考虑明可夫斯基距离
+print("\n考虑明可夫斯基距离")
+best_p = ""
+best_score = 0.0
+best_k = -1
+for k in range(1, 11):
+    for p in range(1, 6):
+        knn_clf = KNeighborsClassifier(n_neighbors=k, weights="distance", p=p)
+        knn_clf.fit(X_train, y_train)
+        score = knn_clf.score(X_test, y_test)
+        if score > best_score:
+            best_k = k
+            best_score = score
+            best_p = p
+print("best_k = ", best_k, "\nbest_score = ", best_score, "\nbest_p = ", best_p)
